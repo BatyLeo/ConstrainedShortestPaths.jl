799437a0a8e828b232158742301dabab754f24ca
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 11:11:36 +0000

    build based on ccf36c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RCSP","category":"page"},{"location":"#RCSP","page":"Home","title":"RCSP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RCSP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RCSP]","category":"page"}]
+}
